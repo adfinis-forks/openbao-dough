0281edd367be197db5ef1cb19759ef7cd8c1fdd9
driif <andrii.fedorchuk@secretz.io>
2025-07-16 08:58:45 +0200

    add -Framework: React 19 /  TS, -Formater/Linter: Biome, -Builder - RSBuild
    Signed-off-by: driif <andrii.fedorchuk@secretz.io>

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+const App = () => {
+  return (
+    <div className="content">
+      <h1>OpenBao Dough</h1>
+      <p>Your new React UI</p>
+    </div>
+  );
+};
+
+export default App;
