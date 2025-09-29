75d4ce6f1244eb0b5ecb7e5c3dc180d5ad99983b
driif <andrii.fedorchuk@secretz.io>
2025-09-29 16:31:54 +0200

    remove unused legacy router. add typescript plugin to openapi-ts configs
    Signed-off-by: driif <andrii.fedorchuk@secretz.io>

--- openapi-ts.config.ts
+++ openapi-ts.config.ts
@@ -3,5 +3,9 @@ import { defineConfig } from '@hey-api/openapi-ts';
 export default defineConfig({
   input: './api/oapi.yaml',
   output: './src/shared/client',
-  plugins: ['@hey-api/client-fetch', '@tanstack/react-query'],
+  plugins: [
+    '@hey-api/typescript',
+    '@hey-api/client-fetch',
+    '@tanstack/react-query',
+  ],
 });
