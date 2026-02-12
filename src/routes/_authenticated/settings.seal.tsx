5678e557bdd4aa39a90d05e55bc01edf070b1906
driif <andrii.fedorchuk@secretz.io>
2026-02-12 13:00:13 +0100

    move to original SealView - minimalistic. Remove system component
    Signed-off-by: driif <andrii.fedorchuk@secretz.io>

--- src/routes/_authenticated/settings.seal.tsx
+++ src/routes/_authenticated/settings.seal.tsx
@@ -1,6 +1,6 @@
 import { createFileRoute } from '@tanstack/react-router';
-import { SystemView } from '../../features/system/SystemView';
+import { SealView } from '@/features/seal/SealView';
 
 export const Route = createFileRoute('/_authenticated/settings/seal')({
-  component: SystemView,
+  component: SealView,
 });
